059bbafeea6590d506f2c2a5f8efd20943ca29e3
Lukasz <exe.mp3@gmail.com>
2018-03-04 10:21:46 +0100

    new structure of app, added controllers, validators

--- server/controllers/index.js
+++ server/controllers/index.js
@@ -1,5 +1,7 @@
 const router = require('express').Router();
+const cors = require('cors');
 
+router.use(cors());
 router.use((req, res, next) => {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
